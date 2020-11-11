ceb1c8f2df5cbef7cc3ec51167d24fb5f123d0f9
Ralf Barkow <>
2020-11-11 14:26:13 +0100

    test (getFilePath): init

---
+++ src/test/java/de/danielluedecke/zettelkasten/database/SettingsTest.java
@@ -0,0 +1,12 @@
+package de.danielluedecke.zettelkasten.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SettingsTest {
+
+    @Test
+    public void getFilePath() {
+    }
+}
